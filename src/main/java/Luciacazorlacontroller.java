4581886345d96f91989b280ece90367fb5a042f1
LuciaCazorla <lucazorlaortega@uma.es>
2025-11-24 16:38:43 +0100

    fix: Estrcutura del repositorio cambiada, pom.xml en la raíz

---
+++ src/main/java/Luciacazorlacontroller.java
@@ -0,0 +1,10 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Luciacazorlacontroller { // Asegúrate de que el nombre de la clase coincide con el nombre del archivo
+    @GetMapping("/ruta-lucia") 
+    public String decirHola() {
+        return "¡Hola Mundo! Este es mi controlador en la rama de desarrollo de Lucía.";
+    }
+}
