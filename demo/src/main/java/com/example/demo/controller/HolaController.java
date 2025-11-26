dd442ec4ffb25fd0d4273458694e3570667b123d
0611111090-uma <0611111090@uma.es>
2025-11-26 13:13:18 +0100

    cambio nombre package

--- demo/src/main/java/com/example/demo/controller/HolaController.java
+++ demo/src/main/java/com/example/demo/controller/HolaController.java
@@ -1,4 +1,4 @@
-package es.grupoB.practica5;
+package es.grupob.practica5;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
