3cefbb02ad89cca8f25ba2dfc85309eae6096839
LuciaCazorla <lucazorlaortega@uma.es>
2025-11-26 09:30:30 +0100

    Cambio de pull request realizado

--- src/main/java/com/example/demo/Luciacazorlacontroller.java
+++ src/main/java/com/example/demo/Luciacazorlacontroller.java
@@ -3,9 +3,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Luciacazorlacontroller { // Asegúrate de que el nombre de la clase coincide con el nombre del archivo
+public class Luciacazorlacontroller { 
     @GetMapping("/ruta-lucia") 
     public String decirHola() {
-        return "¡Hola Mundo! Este es mi controlador en la rama de desarrollo de Lucía.";
+        return "¡Hola Mundo! Este es mi controlador en la rama de desarrollo de Lucía Cazorla.";
     }
 }
